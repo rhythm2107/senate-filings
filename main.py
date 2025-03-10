33ea9e562536fbfbeae33ce2e51fc8b6af0b1865
rhythm2107 <kamil.krzywda.21@gmail.com>
2025-03-10 22:19:50 +0100

    Add core logic to the main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 from modules.scraper_filings import scrape_filings
 from modules.scraper_transactions import scrape_transactions
 from modules.notify_system import send_unnotified_discord_notifications
@@ -6,3 +7,15 @@ from modules.logger import setup_logger
 # Create a logger object for debugging purposes
 logger = setup_logger("main_logger", "main.log")
 
+def main():
+    scrape_filings()
+    time.sleep(2)
+
+    scrape_transactions()
+    time.sleep(2)
+
+    send_unnotified_discord_notifications()
+    time.sleep(2)
+
+if __name__ == "__main__":
+    main()
